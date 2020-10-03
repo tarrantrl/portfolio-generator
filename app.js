9193ea34fc3d637fb40cc8aa81abcdf33c463fab
forsythrl <tarrantrl@utexas.edu>
2020-10-03 14:56:51 -0500

    validate user input

--- app.js
+++ app.js
@@ -13,11 +13,27 @@ const promptUser = () => {
                 type: "input",
                 name: "name",
                 message: "What is your name?",
+                validate: nameInput => {
+                    if (nameInput){
+                        return true;
+                    } else {
+                        console.log("Please enter your name!");
+                        return false;
+                    }
+                }
             },
             {
                 type: "input",
                 name: "github",
                 message: "Enter your GitHub username:",
+                validate: nameInput => {
+                    if (nameInput){
+                        return true;
+                    } else {
+                        console.log("Please enter your GitHub username!");
+                        return false;
+                    }
+                }
             },
             {
                 type: "input",
@@ -42,11 +58,27 @@ Add a New Project
             type: "input",
             name: "name",
             message: "What is the name of your project?",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your project name!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "description",
             message: "Provide a description of the project (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your project description!");
+                    return false;
+                }
+            }
         },
         {
             type: "checkbox",
@@ -58,6 +90,14 @@ Add a New Project
             type: "input",
             name: "link",
             message: "Enter the GitHub link to your project. (Required)",
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your project link!");
+                    return false;
+                }
+            }
         },
         {
             type: "confirm",
